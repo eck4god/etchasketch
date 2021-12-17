d6fd5c846842d83fe7ebecb1e9e5f469a2ea9cb8
Gregory Echelberry <gechelberry@aol.com>
2021-12-17 15:38:30 -0500

    upload

---
+++ etch.js
@@ -0,0 +1,14 @@
+const container = document.querySelector("#container");
+container.className = "grid";
+
+for (let i = 0; i < 16; i++) {
+    let column = document.createElement("div");
+    column.className = "column";
+    for (let j = 0; j < 16; j++) {
+        let cell = document.createElement("div");
+        cell.className = "gridsquare";
+        cell.innerText = ((i * 16) + j) + 1;
+        column.appendChild(cell);
+    }
+    container.appendChild(column);
+}
